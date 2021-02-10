f666f3982cee4b707b7a3ca2e9dfceab869a70b8
Imferal <i.m.feral@gmail.com>
2021-02-10 18:02:21 +0300

    210210_2

--- src/api/api.js
+++ src/api/api.js
@@ -1,32 +1,31 @@
-import axios from "axios"
+import axios from 'axios';
 
-export const getServerData = (type) => {
+// Запрос данных о животных
+export const getServerData = type => {
+  let token = localStorage.getItem ('bearerToken');
+  let getAnimalsUrl = type === 'today'
+    ? 'https://acits-api.herokuapp.com/api/v1/prescriptions/today/'
+    : 'https://acits-api.herokuapp.com/api/v1/animals/';
 
-    let token = localStorage.getItem('bearerToken')
-    let getAnimalsUrl =
-        type === 'today'
-            ? 'https://acits-api.herokuapp.com/api/v1/prescriptions/today/'
-            : 'https://acits-api.herokuapp.com/api/v1/animals/'
+  return axios
+    .get (getAnimalsUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'current-shelter': 1,
+      },
+    })
+    .then (response => response.data.results);
+};
 
-    return axios.get(
-        getAnimalsUrl,
-        {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'current-shelter': 1
-            }
-        })
-        .then(response => response.data.results)
-}
-
+// Авторизация
 export const getToken = (login, password) => {
-    return axios
-        .post(
-            'https://acits-api.herokuapp.com/api/token/',
-            {
-                username: login,
-                password: password,
-            }
-        )
-        .then(response => response.data.access)
-}
+  return axios
+    .post ('https://acits-api.herokuapp.com/api/token/', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      username: login,
+      password: password,
+    })
+    .then (response => response.data.access);
+};
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,20 @@
 import Header from './../Header/Header';
-import { Route, BrowserRouter } from "react-router-dom";
+import {Route, BrowserRouter} from 'react-router-dom';
 import LoginContainer from '../Login/LoginContainer';
 import TodayContainer from '../Today/TodayContainer';
 import AnimalsContainer from '../Animals/AnimalsContainer';
 import Auth from '../Auth/Auth';
 
 const App = () => {
-    return (
-        <BrowserRouter basename="/acits">
-            <Header />
-            <Route exact path="/" component={Auth} />
-            <Route exact path="/animals" component={AnimalsContainer} />
-            <Route exact path="/today" component={TodayContainer} />
-            <Route path="/login" component={LoginContainer} />
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter basename="/acits">
+      <Header />
+      <Route exact path="/" component={Auth} />
+      <Route exact path="/animals" component={AnimalsContainer} />
+      <Route exact path="/today" component={TodayContainer} />
+      <Route path="/login" component={LoginContainer} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
